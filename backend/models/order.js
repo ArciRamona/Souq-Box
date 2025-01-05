84f29108ffd305928128bdba73ce3716a428d2d4
iamarciad <gladysando0206@gmail.com>
2025-01-06 00:19:29 +0300

    Creating Order Model

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,39 +1,101 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-  shippingInfo: {
-    address: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    phoneNo: {
-      type: String,
-      required: true,
-    },
-    zipCode: {
-      type: String,
-      required: true,
-    },
-    country: {
-      type: String,
-      required: true,
-    },
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  orderItems: [
-    {
-      name: {
+const orderSchema = new mongoose.Schema(
+  {
+    shippingInfo: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      phoneNo: {
+        type: String,
+        required: true,
+      },
+      zipCode: {
+        type: String,
+        required: true,
+      },
+      country: {
         type: String,
         required: true,
       },
     },
-  ],
-});
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: String,
+          required: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+      },
+    ],
+    paymentMethod: {
+      type: String,
+      required: [true, "Please select a payment method"],
+      enum: {
+        values: ["COD", "Card"],
+        message: "Please select: COD, Card",
+      },
+    },
+    paymentInfo: {
+      id: String,
+      status: String,
+      amount: Number,
+      currency: String,
+      created_at: Date,
+    },
+    itemPrice: {
+      type: Number,
+      required: true,
+    },
+    taxAmount: {
+      type: Number,
+      require: true,
+    },
+    shippingAmount: {
+      type: Number,
+      require: true,
+    },
+    totalAmount: {
+      type: Number,
+      require: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: {
+        values: ["Processing", "Shipped", "Delivered"],
+        message: "Please select correct order status",
+      },
+      default: "Processing",
+    },
+    deliveredAt: Date,
+  },
+  { timeseries: true }
+);
+
+export default mongoose.model("Order", orderSchema);
