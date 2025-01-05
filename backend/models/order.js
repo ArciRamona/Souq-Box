e8b827f3d388e6d3db27b280ba368f60642fc452
iamarciad <gladysando0206@gmail.com>
2025-01-05 23:51:04 +0300

    Unfinished Creating Order Model

---
+++ backend/models/order.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  shippingInfo: {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phoneNo: {
+      type: String,
+      required: true,
+    },
+    zipCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  orderItems: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
