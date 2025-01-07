1c8e72f228343db504a16551534852b9d21f113e
iamarciad <gladysando0206@gmail.com>
2025-01-07 03:21:26 +0300

    Get Single Order & Logged in User Orders

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,3 +1,5 @@
+// Handle Orders Resource
+// Create Order Model
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import Order from "../models/order.js";
 import { getResetPasswordTemplate } from "../utils/emailTemplates.js";
@@ -30,5 +32,32 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     order,
+  });
+});
+
+// Get Single Order & Logged in User Order
+// Create two Endpoints for the order. One is to get the order details by ID. And one is to get the orders of the current user.
+// Get order deatails =>  /api/v1/orders/:id
+export const getOrderDeatails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  ); // populate the order details with user details, from "user" we get the entire order details from name email id etc. we only need name and email
+
+  if (!order) {
+    return next(new ErrorHandler("No Order found with this ID", 400));
+  }
+
+  res.status(200).json({
+    order,
+  });
+});
+
+// Get orders of current user => /api/v1/orders/me/orders
+export const myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    orders,
   });
 });
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { isAuthenticatedUser } from "../middlewares/auth.js";
-import { newOrder } from "../controllers/orderControllers.js";
+import {
+  getOrderDeatails,
+  myOrders,
+  newOrder,
+} from "../controllers/orderControllers.js";
 
 const router = express.Router();
 
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
+router.route("/orders/:id").get(isAuthenticatedUser, getOrderDeatails);
+router.route("/me/orders").get(isAuthenticatedUser, myOrders);
 
 export default router;
