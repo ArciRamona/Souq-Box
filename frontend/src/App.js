41931f62be1a5ef39ef5b3400652784fe0401de3
iamarciad <gladysando0206@gmail.com>
2025-01-15 21:58:34 +0300

    SouqBoxIT HTML & CSS

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.jsx";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header/>
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
