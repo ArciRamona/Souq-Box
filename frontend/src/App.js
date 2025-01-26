2b67e8136621186bf4a2de9f6ec8c69e914b3de5
iamarciad <gladysando0206@gmail.com>
2025-01-26 23:58:04 +0300

    Loader Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,24 @@
-
 import Footer from "./components/layout/Footer.jsx";
 import Header from "./components/layout/Header.jsx";
 import "./App.css";
 import Home from "./components/Home.jsx";
 
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-        <div className="App">
-            <Header/>
-            <div className="container">
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    {/* Add more routes here */}
-                </Routes>
-            </div>
-          
-            <Footer/>
-      
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {/* Add more routes here */}
+          </Routes>
         </div>
+
+        <Footer />
+      </div>
     </Router>
   );
 }
---
+++ frontend/src/components/layout/Loader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loader = () => {
+  return <div className="looder">Loader</div>;
+};
+
+export default Loader;
