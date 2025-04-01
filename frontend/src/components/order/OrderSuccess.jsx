1c83a4c0a844076b9c5ad41786739c6531010681
iamarciad <gladysando0206@gmail.com>
2025-04-01 22:00:43 +0300

    Place on Successful Payment

--- frontend/src/components/order/OrderSuccess.jsx
+++ frontend/src/components/order/OrderSuccess.jsx
@@ -1,23 +1,23 @@
-// import React from "react";
-// import MetaData from "../layout/MetaData";
-// import { Link } from "react-router-dom";
+import React from "react";
+import MetaData from "../layout/MetaData";
+import { Link } from "react-router-dom";
 
-// const OrderSuccess = () => {
-//   return (
-//     <>
-//       <MetaData title="Order Successful" />
-//       <div className="container text-center py-5">
-//         <h1 className="text-success mb-4">🎉 Payment Successful!</h1>
-//         <p className="lead">
-//           Thank you for your order. Your payment was successful, and we’re
-//           preparing your items.
-//         </p>
-//         <Link to="/orders/me" className="btn btn-primary mt-4">
-//           View My Orders
-//         </Link>
-//       </div>
-//     </>
-//   );
-// };
+const OrderSuccess = () => {
+  return (
+    <>
+      <MetaData title="Order Successful" />
+      <div className="container text-center py-5">
+        <h1 className="text-success mb-4">🎉 Payment Successful!</h1>
+        <p className="lead">
+          Thank you for your order. Your payment was successful, and we’re
+          preparing your items.
+        </p>
+        <Link to="/orders/me" className="btn btn-primary mt-4">
+          View My Orders
+        </Link>
+      </div>
+    </>
+  );
+};
 
-// export default OrderSuccess;
+export default OrderSuccess;
